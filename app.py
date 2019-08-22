767f739c5a1905365b072d2e43c61ffae9de2979
alex <jthu4alex@163.com>
2019-08-22 17:52:02 +0800

    2019/8/22 v0.0.1
    1\ the base frame was built;

---
+++ app.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# @Time    : 8/22/19 5:48 PM
+# @Author  : Alex Hu
+# @Contact : jthu4alex@163.com
+# @FileName: app.py
+# @Software: PyCharm
+# @Blog    : http://www.gzrobot.net/aboutme
+# @version : 0.0.0
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
