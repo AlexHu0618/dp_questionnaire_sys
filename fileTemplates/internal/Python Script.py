06a16aab832218d4b2ff2716524f3191cb54f855
alex <jthu4alex@163.com>
2019-08-22 17:38:24 +0800

    PY-2018.1.1 <alex@localhost Create ide.general.xml, project.default.xml, other.xml, jdk.table.xml, laf.xml, Default.xml, find.xml, ui.lnf.xml, filetypes.xml, Python Script.py, vcs.xml, editor.codeinsight.xml, colors.scheme.xml, databaseSettings.xml, git.xml, databaseDrivers.xml, editor.xml, github_settings.xml, debugger.xml

---
+++ fileTemplates/internal/Python Script.py
@@ -0,0 +1,10 @@
+##!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : ${DATE} ${TIME}
+# @Author  : Alex Hu
+# @Contact : jthu4alex@163.com
+# @FileName: ${NAME}.py
+# @Software: ${PRODUCT_NAME}
+# @Blog    : http://www.gzrobot.net/aboutme
+# @version : 0.0.0
+
