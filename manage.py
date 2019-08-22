c50c9e3c918e864f0a70c88a2f43b3ad3c0a255b
alex <jthu4alex@163.com>
2019-08-22 18:36:55 +0800

    2019/8/22 v0.0.2
    1\ set uWSGI as web server;
    2\ set run.sh as start command;

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 # @Time    : 8/22/19 5:48 PM
 # @Author  : Alex Hu
 # @Contact : jthu4alex@163.com
-# @FileName: app.py
+# @FileName: manage.py
 # @Software: PyCharm
 # @Blog    : http://www.gzrobot.net/aboutme
 # @version : 0.0.0
@@ -18,4 +18,4 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', debug=True)
---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# This script was built for starting run the python program using uWSGI
+
+echo Starting...
+
+uwsgi --http :5002 --gevent 1000 --http-websockets --wsgi-file manage.py --callable app
